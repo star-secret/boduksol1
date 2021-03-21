58bfb86a5f0f32db003ff2beff53b854a3f021ee
JHPARK-air <boduksol23@ajou.ac.kr>
2021-03-21 20:59:20 +0900

    final practice

--- pjh/sand.py
+++ pjh/sand.py
@@ -1,6 +1,3 @@
-
-
-
 x, y = map(int,(input().split()))
 a, b, c, d = map(int,(input().split()))
 n = int(input())
@@ -57,31 +54,3 @@ for i in range(n):
         print(dist[i])
         print(x_list[i],y_list[i])
 
-
-h, w = input().split()
-
-
-first_list=[]
-# [[0 0 0 0][0 0 0 0][0 0 0 0][0 0 0 0]]
-for i in range(h+1):
-    first_list.append([])
-    for j in range(w+1):
-        first_list[i].append(0)
-
-n = map(int, input())
-
-for i in range(n):
-    l, d, x, y = map(int, input.split())
-    x-=1
-    y-=1
-    for j in range(l):
-        if d==0:
-            first_list[x][y+j]=1
-        else:
-            first_list[x+j][y]=1
-
-for i in range(h+1):
-    for j in range(w+1):
-        print(first_list[i][j], end='')
-    print()
-    
---
+++ pjh/sugar.py
@@ -0,0 +1,27 @@
+h, w = input().split()
+
+
+first_list=[]
+# [[0 0 0 0][0 0 0 0][0 0 0 0][0 0 0 0]]
+for i in range(h+1):
+    first_list.append([])
+    for j in range(w+1):
+        first_list[i].append(0)
+
+n = map(int, input())
+
+for i in range(n):
+    l, d, x, y = map(int, input.split())
+    x-=1
+    y-=1
+    for j in range(l):
+        if d==0:
+            first_list[x][y+j]=1
+        else:
+            first_list[x+j][y]=1
+
+for i in range(h+1):
+    for j in range(w+1):
+        print(first_list[i][j], end='')
+    print()
+    
