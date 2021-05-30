ecf1c596b324932a417bf448d21d05f9c099e5e8
star-secret <kkjh4357@gmail.com>
2021-05-30 23:06:09 +0900

    second project

---
+++ pythonProject2/sub code/main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#import circle
+
+print("HI")
+
+#circle.make_circle(4,5)
+
+
+#a = areasplit.makearea()
+#areasplit.splitarea()
+
+
+
+
+
+#x 따로 y 따로 받았으면 좋겠다.
